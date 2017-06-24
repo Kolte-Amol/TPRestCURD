9567ae3921c038b0efc30dd442f917018d7cde86
kolteamol <mack37@gmail.com>
2017-06-25 00:06:42 +0530

    Application class modified

--- UserManagement/src/main/java/com/rest/curd/UserManagementApplication.java
+++ UserManagement/src/main/java/com/rest/curd/UserManagementApplication.java
@@ -2,8 +2,9 @@ package com.rest.curd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication (exclude = SecurityAutoConfiguration.class)
 public class UserManagementApplication {
 
 	public static void main(String[] args) {
